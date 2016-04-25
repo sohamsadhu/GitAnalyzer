17a3a982f9adafe9770f34cd2ac128db150f9c02
Soham Sadhu <sohamsadhu@gmail.com>
2016-04-25 16:47:43 -0400

    Format and limit scope of test dependencies

--- src/main/java/com/soham/gitanalyzer/GitHubAnalyzer.java
+++ src/main/java/com/soham/gitanalyzer/GitHubAnalyzer.java
@@ -12,13 +12,13 @@ import org.eclipse.egit.github.core.service.ContentsService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class GitHubAnalyzer {
-	
+
 	private RepositoryService repositoryService;
-	private ContentsService   contentsService;
-	
+	private ContentsService contentsService;
+
 	public GitHubAnalyzer() {
 		this.repositoryService = new RepositoryService();
-		this.contentsService   = new ContentsService();
+		this.contentsService = new ContentsService();
 	}
 
 	public Repository getRepository(String owner, String repositoryName) {
@@ -26,126 +26,125 @@ public class GitHubAnalyzer {
 		try {
 			repository = repositoryService.getRepository(owner, repositoryName);
 		} catch (IOException ioex) {
-			System.err.println("Could not locate repository:"+ repositoryName +" for owner:"+owner);
+			System.err.println("Could not locate repository:" + repositoryName + " for owner:" + owner);
 			ioex.printStackTrace();
 		}
 		return repository;
 	}
-	
-	public List<RepositoryBranch> getBranches(Repository repository){
+
+	public List<RepositoryBranch> getBranches(Repository repository) {
 		List<RepositoryBranch> branches = null;
 		try {
 			branches = repositoryService.getBranches(repository);
 		} catch (IOException ioex) {
-			System.err.println("Could not locate branches for repository:"+ repository.getName());
+			System.err.println("Could not locate branches for repository:" + repository.getName());
 			ioex.printStackTrace();
 		}
 		return branches;
 	}
-	
+
 	public List<RepositoryTag> getTags(Repository repository) {
 		List<RepositoryTag> tags = null;
 		try {
 			tags = repositoryService.getTags(repository);
 		} catch (IOException ioex) {
-			System.err.println("Could not locate tags for repository:"+ repository.getName());
+			System.err.println("Could not locate tags for repository:" + repository.getName());
 			ioex.printStackTrace();
 		}
 		return tags;
 	}
-	
+
 	public List<RepositoryContents> getContents(Repository repository, String path, String reference) {
-	    List<RepositoryContents> contents = null;
-	    try {
-            contents = contentsService.getContents(repository, path, reference);
-        } catch (IOException ioex) {
-            System.err.println("Contents for repository "+ repository.getName() +" for tag/branch "
-                    + reference +" on path "+ ((path == null)? "root" : path) +" could not be found.");
-            ioex.printStackTrace();
-        }
-	    return contents;
+		List<RepositoryContents> contents = null;
+		try {
+			contents = contentsService.getContents(repository, path, reference);
+		} catch (IOException ioex) {
+			System.err.println("Contents for repository " + repository.getName() + " for tag/branch " + reference
+					+ " on path " + ((path == null) ? "root" : path) + " could not be found.");
+			ioex.printStackTrace();
+		}
+		return contents;
 	}
-	
-	public List<String> notContainsFile(Repository repository, List<String> references, String path,
-	        String fileName) {
-	    List<String> referenceNames = new ArrayList<String>();
-	    boolean notFound;
-	    for(String reference: references) {
-	        List<RepositoryContents> contents = getContents(repository, null, reference);
-	        notFound = true;
-	        if(contents != null) {
-	            for(RepositoryContents content: contents) {
-	                if(content.getName().equals(fileName)) {
-	                    notFound = false;
-	                    break;
-	                }
-	            }
-	            if(notFound) {
-	                referenceNames.add(reference);
-	            }
-	        }
-	    }
-	    return referenceNames;
+
+	public List<String> notContainsFile(Repository repository, List<String> references, String path, String fileName) {
+		List<String> referenceNames = new ArrayList<String>();
+		boolean notFound;
+		for (String reference : references) {
+			List<RepositoryContents> contents = getContents(repository, null, reference);
+			notFound = true;
+			if (contents != null) {
+				for (RepositoryContents content : contents) {
+					if (content.getName().equals(fileName)) {
+						notFound = false;
+						break;
+					}
+				}
+				if (notFound) {
+					referenceNames.add(reference);
+				}
+			}
+		}
+		return referenceNames;
 	}
-	
+
 	public void analyze() {
 		Repository repository = getRepository("ruby", "ruby");
 		String fileNotContains = new String(".travis.yml");
-		if(repository != null){
-		    System.out.println("Repository URL is: "+ repository.getHtmlUrl());
-			List<RepositoryBranch> branches       = getBranches(repository);
-		    List<RepositoryTag>    tags           = getTags(repository);
-		    List<String> branchNamesNotHavingFile = null;
-		    List<String> tagNamesNotHavingFile    = null;
-			if(branches != null) {
+		if (repository != null) {
+			System.out.println("Repository URL is: " + repository.getHtmlUrl());
+			List<RepositoryBranch> branches = getBranches(repository);
+			List<RepositoryTag> tags = getTags(repository);
+			List<String> branchNamesNotHavingFile = null;
+			List<String> tagNamesNotHavingFile = null;
+			if (branches != null) {
 				System.out.println("====================================");
-				System.out.println("Repository: "+ repository.getName() +" has following branches.");
+				System.out.println("Repository: " + repository.getName() + " has following branches.");
 				String branchName;
 				List<String> branchNames = new ArrayList<>();
-				for(RepositoryBranch branch: branches) {
-				    branchName = branch.getName();
+				for (RepositoryBranch branch : branches) {
+					branchName = branch.getName();
 					System.out.println(branchName);
 					branchNames.add(branchName);
 				}
 				branchNamesNotHavingFile = notContainsFile(repository, branchNames, "null", fileNotContains);
 			}
-			if(tags != null) {
+			if (tags != null) {
 				System.out.println("====================================");
-				System.out.println("Repository: "+ repository.getName() +" has following tags.");
+				System.out.println("Repository: " + repository.getName() + " has following tags.");
 				String tagName;
 				List<String> tagNames = new ArrayList<>();
-				for(RepositoryTag tag: tags) {
-				    tagName = tag.getName();
+				for (RepositoryTag tag : tags) {
+					tagName = tag.getName();
 					System.out.println(tagName);
 					tagNames.add(tagName);
 				}
 				tagNamesNotHavingFile = notContainsFile(repository, tagNames, "null", fileNotContains);
 			}
-			if((branchNamesNotHavingFile == null) || (0 == branchNamesNotHavingFile.size())) {
-			    System.out.println("====================================");
-                System.out.println("All the branches in repository "+ repository.getName() +
-                        " have the file "+ fileNotContains);
+			if ((branchNamesNotHavingFile == null) || (0 == branchNamesNotHavingFile.size())) {
+				System.out.println("====================================");
+				System.out.println(
+						"All the branches in repository " + repository.getName() + " have the file " + fileNotContains);
 			} else {
-			    System.out.println("====================================");
-                System.out.println("Branches that do not have "+ fileNotContains +" in root folder are");
-                for(String branch : branchNamesNotHavingFile) {
-                    System.out.println(branch);
-                }
+				System.out.println("====================================");
+				System.out.println("Branches that do not have " + fileNotContains + " in root folder are");
+				for (String branch : branchNamesNotHavingFile) {
+					System.out.println(branch);
+				}
 			}
-			if((tagNamesNotHavingFile == null) || (0 == tagNamesNotHavingFile.size())) {
-                System.out.println("====================================");
-                System.out.println("All the tags in repository "+ repository.getName() +
-                        " have the file "+ fileNotContains);
-            } else {
-                System.out.println("====================================");
-                System.out.println("Tags that do not have "+ fileNotContains +" in root folder are");
-                for(String tag : tagNamesNotHavingFile) {
-                    System.out.println(tag);
-                }
-            }
+			if ((tagNamesNotHavingFile == null) || (0 == tagNamesNotHavingFile.size())) {
+				System.out.println("====================================");
+				System.out.println(
+						"All the tags in repository " + repository.getName() + " have the file " + fileNotContains);
+			} else {
+				System.out.println("====================================");
+				System.out.println("Tags that do not have " + fileNotContains + " in root folder are");
+				for (String tag : tagNamesNotHavingFile) {
+					System.out.println(tag);
+				}
+			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		GitHubAnalyzer gitHubAnalyzer = new GitHubAnalyzer();
 		gitHubAnalyzer.analyze();
